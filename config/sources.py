6c098bc07b80c35881531cc7332a0e735535ad1d
Edson Copque <ecop@disroot.org>
2025-03-24 16:15:22 -0300

    update.

---
+++ config/sources.py
@@ -0,0 +1,11 @@
+# FILE: /config/sources.py
+
+NEW_SOURCES = [
+    #RSS feed:
+    "http://g1.globo.com/dynamo/rss2.xml",
+    "https://www.bbc.com/portuguese/brasil/index.xml",
+
+    #Scraping sites:
+    "https://www.uol.com.br/",
+    "https://www.tecmundo.com.br/"
+]
