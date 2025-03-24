f50c89db0c9869f3716e1a2478dc7ebd1ac4ecfc
Edson Copque <ecop@disroot.org>
2025-03-24 17:47:45 -0300

    update.

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,13 @@
 # FILE: /dashboard/app.py
 
 # [Dashboard with Streamlt]:
+import streamlit as st #32:
+import pandas as pd
+from scripts.collector import NewsCollector #33:
+from config.sources import NEW_SOURCES
+
+# Page setup:
+st.set_page_config(page_title='News Analyzer', layout='wide')
+st.title('News Analyzer with DeepSeek')
+
+# Sidebar for settings:
