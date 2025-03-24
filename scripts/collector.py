87a70921d9bc38f456ad7a6575a3bbc30c9c2996
Edson Copque <ecop@disroot.org>
2025-03-24 16:34:27 -0300

    update.

--- scripts/collector.py
+++ scripts/collector.py
@@ -1,3 +1,11 @@
 # FILE: /scripts/collector.py
 
 # [News Collector Implementation]:
+import requests
+from bs4 import BeautifulSoup
+import feedparser
+import pandas as pd
+from datetime import datetime
+import time
+from urllib.parse import urljoin
+
