bf4b9169c9a4b287bee9610ed7f087f41e3382f9
Edson Copque <ecop@disroot.org>
2025-03-24 16:39:39 -0300

    update.

--- scripts/collector.py
+++ scripts/collector.py
@@ -9,3 +9,24 @@ from datetime import datetime
 import time
 from urllib.parse import urljoin
 
+class NewsCollector:
+    def __init__(self):
+        self.headers = {'User-Agent': 'Mozilla/5.0'}
+
+    def scrape_site(self, url):
+        try:
+            response = requests.get(url, headers=self.headers, timeout=10)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            articles = list()
+
+            for i1 in soup.select('article')[:10]:
+                title = i1.find('h2').text.strip() if i1.find('h2') else 'Untitled'
+                link = i1.find('a')['href'] if i1.find('a') else '#'
+                link = urljoin(url, link)
+
+                articles.append({
+                    'title': title,
+                    'url': link,
+                    'source': url,
+                    'date': datetime.now().strftime('%Y-%m-%d')
+                })
