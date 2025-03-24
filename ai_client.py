2b2c7295bd1ec0848a7b824d0bd5027613587804
Edson Copque <ecop@disroot.org>
2025-03-24 17:36:33 -0300

    update.

--- ai_client.py
+++ ai_client.py
@@ -1,3 +1,13 @@
 # FILE: /ai_client.py
 
 # [DeepSeek]:
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class DeepSeekAI:
+    def __init__(self):
+        self.api_key = os.getenv('DEEPSEEK_API_KEY')
+        self.base_url = 'https://api.deepseek.com/v1/chat/completions'
