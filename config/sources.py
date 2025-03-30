fa497d7fc21693215e4328844ab4d2a75f1e9dd7
Edson Copque <ecop@disroot.org>
2025-03-30 17:27:40 -0300

    update.

--- config/sources.py
+++ config/sources.py
@@ -1,12 +1,12 @@
 # FILE: /config/sources.py
 
-# [News Sources]: #3:
+# [News Sources]
 NEWS_SOURCES = [
-    #RSS feed:
+    #RSS feed
     "http://g1.globo.com/dynamo/rss2.xml",
     "https://www.bbc.com/portuguese/brasil/index.xml",
 
-    #Scraping sites:
+    #Scraping sites
     "https://www.uol.com.br/",
     "https://www.tecmundo.com.br/"
 ]
